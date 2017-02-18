866f9059ec06f7658b2ee3c47fd20633948325b7
Jørgen L. Hansen <Jrgen L. Hansen>
2017-02-18 08:31:02 +0100

    added gmaps api and some files

--- main.js
+++ main.js
@@ -18,3 +18,42 @@ $(document).ready(function () {
   });
 
 });
+
+// get data from ./data/Lokale_kulturminner.json -> features.geometry.x && features.geometry.y
+
+/**
+    features[0].attributes.Navn = "GILLSVANNET 32 C3.1"
+    features[0].features.x = 442577
+    features[0].features.y = 6449945
+
+    for
+*/
+
+function getBikeMaps() {
+    var bike_POI_list = [];
+    var jsonPath = "./data/Sykkelkart_interessepunkt.json";
+    for(var bikeMap in jsonPath.features.){
+
+
+        var bikeMapObject = {
+            x = bikeMap.geometry.x;
+            y = bikeMap.geometry.y;
+            name = bikeMap.attributes.Navn;
+        }
+
+        bike_POI_list[bikeMapObject];
+    }
+    return bike_POI_list;
+}
+
+function getCurrentLocation() {
+    var currentPosition;
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+      do_something(position.coords.latitude, position.coords.longitude);
+    });
+    } else {
+      console.log("geolocation is not available");
+    }
+
+}
