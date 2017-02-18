21a3c76f69df9bdf6eb3d548231c2b1d0f9dde9d
Jørgen L. Hansen <Jrgen L. Hansen>
2017-02-18 07:54:06 +0100

    added explorer file and maps api

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  var map = new GMaps({
+      div: '#map',
+      lat: -12.043333,
+      lng: -77.028333
+  });
+
+  map.addMarker({
+    lat: -12.043333,
+    lng: -77.028333,
+    title: 'Lima',
+    click: function(e) {
+      infoWindow: {
+        content: '<p>HTML Content</p>'
+      }
+    }
+
+  });
+
+});
